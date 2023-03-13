5572df1b21b86d8dc6637c6cec326259b96f6054
Majid Ali <majidalipahore55566@gmail.com>
2023-03-13 06:52:13 -0700

    server.js modified

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.get("/", (req, res, next) => {
 app.get("/products", (req, res, next) => {
   res.json({ message: "Products" });
 });
+app.use("/orders", (req, res, next) => {
+  res.json({ message: "orders" });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
